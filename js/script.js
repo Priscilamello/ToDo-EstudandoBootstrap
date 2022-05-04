c953b554d67d72885531dd4528976ef8e2b33e2b
Priscilamello <priscilamello.8@gmail.com>
2022-05-04 11:04:25 -0300

    construção da função que recebe input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const input = document.querySelector('#entrada')
-const assBtn = document.querySelector('.inputField button')
+const inputBox = document.querySelector('#entrada')
+const addBtn = document.querySelector('.inputField button')
 const todoList = document.querySelector('.todoList')
 const deleteAllBtn = document.querySelector('.footer button')
 
+inputBox.onkeyup = _ => {
+    let userData = inputBox.value //Esta variável pega o valor de entrada do usuário
+    if (userData.trim() != 0){    //se os valores do usuário não forem apenas espaços
+        addBtn.classList.add('active')
+    }else{
+        addBtn.classList.remove('active')
+
+    }
+
+}
