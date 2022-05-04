4c24f343030c54933bbd8a5a9c55679ab8054779
Priscilamello <priscilamello.8@gmail.com>
2022-05-04 10:25:31 -0300

    criando variáveis globais

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('#entrada')
+const assBtn = document.querySelector('.inputField button')
+const todoList = document.querySelector('.todoList')
+const deleteAllBtn = document.querySelector('.footer button')
+
