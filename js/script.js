e6baf0ca338e45270c6e9ebb67f2f53b5c769b96
Priscilamello <priscilamello.8@gmail.com>
2022-05-04 13:51:43 -0300

    Criando função que deleta uma tarefa

--- js/script.js
+++ js/script.js
@@ -3,56 +3,66 @@ const addBtn = document.querySelector('.inputField button')
 const todoList = document.querySelector('.todoList')
 const deleteAllBtn = document.querySelector('.footer button')
 
+//arrow function
+
 inputBox.onkeyup = _ => {
-    let userData = inputBox.value //Esta variável pega o valor de entrada do usuário
-    if (userData.trim() != 0){    //se os valores do usuário não forem apenas espaços
-        addBtn.classList.add('active')
-    }else{
-        addBtn.classList.remove('active')
-
-    }
-
+  let userData = inputBox.value //Esta variável pega o valor de entrada do usuário
+  if (userData.trim() != 0) { //Se os valores do usuário não forem apenas espaços
+    addBtn.classList.add('active')
+  }else{
+    addBtn.classList.remove('active')
+  }
 }
+
+showTasks()
 
 addBtn.onclick = _ => {
-    let userData = inputBox.value
-    let getLocalStorage = localStorage.getItem ('New Todo') // esta variável pega o armazenamento local
-    if (getLocalStorage == null){  //vamos verificar se o armazenamento local for nulo
-        listArr = [] //vamos criar um array em branco
-
-    }else{
-        listArr = JSON.parse(getLocalStorage) //estamos empurrando ou add dados deo usuário à nossa lista
-    }
-    listArr.push(userData) // empurrando para dentro do array os dados do usuário
-    localStorage.setItem('New Todo', JSON.stringify (listArr)) // estamos transformando um ob js em um fragmento js
+  let userData = inputBox.value
+  let getLocalStorage = localStorage.getItem('New Todo') //Esta variável vai pegar o armazenamento local
+  if(getLocalStorage == null) { // vamos verificar se o armazenamento local for nulo
+    listArr = [] // vamos criar um array em branco
+  }else{
+    listArr = JSON.parse(getLocalStorage) //estamos empurrando ou adicionando dados do usuário à nossa lista
+  }
+  listArr.push(userData) //Empurrando para dentro do array os dados do usuário
+  localStorage.setItem('New Todo', JSON.stringify(listArr)) //Estamos transformando um objeto js em um fragmento js
+  showTasks() //chamando a função showTasks
 }
 
-
 //função para adicionar tarefas dentro da lista
-function showTasks(){
-    let getLocalStorage = localStorage.getItem('New Todo')// esta variável pega o armazenamento local
-    if(getLocalStorage == null){
-        listArr = []
-    }else{
-        listArr = JSON.parse(getLocalStorage)
-    }
-    const pendingNum = document.querySelector('.pendingNum')
-    pendingNum.textContent = listArr.length
+function showTasks() {
+  let getLocalStorage = localStorage.getItem('New Todo') //Esta variável vai pegar o armazenamento local
+  if (getLocalStorage == null) {
+    listArr = []
+  }else{
+    listArr = JSON.parse(getLocalStorage)
+  }
+  const pendingNum = document.querySelector('.pendingNum')
+  pendingNum.textContent = listArr.length
 
-    if(listArr.length > 0){ //se o tamanho do array for maior que 0
-        deleteAllBtn.classList.add('active')//ativa a classe active
-    }else{
-        deleteAllBtn.classList.remove('active')//desativa a classe active
-    }
+  if(listArr.length > 0) { // Se o tamanho do array for maior que 0
+    deleteAllBtn.classList.add('active') //ativa a classe active
+  }else{
+    deleteAllBtn.classList.remove('active') //desativa a classe active
+  }
 
-    let newLiTag = ''
-    addBtn.classList.remove('active')
-    listArr.forEach((element, index) => {
-        newLiTag += `
-        <li> ${element} <span onclick = "deleteTask()"></span></li>
-        ` 
-        
-    })
-    todoList.innerHTML = newLitag // add uma nova li dentro da nossa lista no HTML
-    inputBox.value = '' //uma vez adicionada a tarefa deixe o campo entrada em branco
+  let newLiTag = ''
+  addBtn.classList.remove('active')
+  listArr.forEach((element, index) => {
+    newLiTag += `
+    <li>${element}<span onclick="deleteTask(${index})"></span></li>
+    `
+  }) 
+  todoList.innerHTML = newLiTag // Adicionando uma nova li dentro da nossa lista no HTML
+  inputBox.value = '' // uma vez adicionada a tarefa deixe o campo entrada em branco
+}
+
+//função para deletar uma tarefa específica
+function deleteTask(index) {
+  let getLocalStorage = localStorage.getItem('New Todo')
+  listArr = JSON.parse(getLocalStorage)
+  listArr.splice(index, 1) // exclui ou remove um determinado item indexado
+  //? depois de remover o item atualize noivamente o armazenamento local
+  localStorage.setItem('New Todo', JSON.stringify(listArr))
+  showTasks()
 }
